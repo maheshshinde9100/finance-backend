775ebf19e06d5ff7c26598532fa2a963cdea5828
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-04-03 12:13:00 +0530

    user and role model commit

---
+++ src/main/java/com/mahesh/zorvyn/model/User.java
@@ -0,0 +1,38 @@
+package com.mahesh.zorvyn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    
+    @Id
+    private String id;
+    
+    @Indexed(unique = true)
+    private String username;
+    
+    @Indexed(unique = true)
+    private String email;
+    
+    private String password;
+    
+    private Set<Role> roles = new HashSet<>();
+    
+    private boolean active = true;
+    
+    private LocalDateTime createdAt = LocalDateTime.now();
+    
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
