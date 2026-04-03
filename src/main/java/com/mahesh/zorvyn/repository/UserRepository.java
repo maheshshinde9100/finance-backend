53121d35b0791db5d9bf57ca31470d81ce593fdd
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-04-03 12:38:29 +0530

    userRepository - db layer

---
+++ src/main/java/com/mahesh/zorvyn/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mahesh.zorvyn.repository;
+
+import com.mahesh.zorvyn.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existsByUsername(String username);
+}
