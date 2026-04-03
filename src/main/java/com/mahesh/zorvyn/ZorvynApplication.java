fd94f43b23a50c2e686afb57b5c80e56bbe1769f
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-04-03 12:05:12 +0530

    intial commit for finance-backend assesment by zorvin-fintech

---
+++ src/main/java/com/mahesh/zorvyn/ZorvynApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.zorvyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorvynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZorvynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/zorvyn/ZorvynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.zorvyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorvynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
