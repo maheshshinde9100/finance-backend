0d71cfa208590515a042d1dbcfd58df697a84b7b
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-04-03 12:39:14 +0530

    add existByEmail() in userRepository

--- src/main/java/com/mahesh/zorvyn/repository/UserRepository.java
+++ src/main/java/com/mahesh/zorvyn/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
